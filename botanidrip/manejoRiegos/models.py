e4894a8dee091e7825b2d9712ea7b6b4cbfb2c3f
CHOP4NCHO <pancho.pantoja@outlook.es>
2023-12-05 11:57:12 -0300

    creacion de modelo riego

---
+++ botanidrip/manejoRiegos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+# Create your models here.
+class RiegoPrueba(models.Model):
+    riegoid = models.IntegerField(primary_key=True)
+    fecha = models.DateField()
+    hora = models.CharField(max_length=10)
+    automatico = models.BooleanField()
+    
